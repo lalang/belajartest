df112e2e99fadf99040be6e55003ac72872ee4df
Abedniego Satrio Wenang <niegossatrio@gmail.com>
2015-09-07 04:39:07 +0700

    button prev next tab

---
+++ backend/models/base/Params.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace backend\models\base;
+
+use Yii;
+
+/**
+ * This is the base model class for table "params".
+ *
+ * @property string $name
+ * @property string $value
+ */
+class Params extends \yii\db\ActiveRecord
+{
+
+    use \mootensai\relation\RelationTrait;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'params';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'name' => Yii::t('app', 'Name'),
+            'value' => Yii::t('app', 'Value'),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return \app\models\ParamsQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \app\models\ParamsQuery(get_called_class());
+    }
+}
