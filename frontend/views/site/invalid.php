481be8470fb1995b55152f0cb926c104777aab5f
Revan <revanreginal@gmail.com>
2015-09-18 16:23:20 +0700

    - Valid / Invalid page

--- frontend/views/site/invalid.php
+++ frontend/views/site/invalid.php
@@ -4,7 +4,17 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 $this->context->layout = 'main-no-landing';
 ?>
-<div class="row">
-    <p>Data perizinan yang anda cek tidak ada dalam database kami, silahkan untuk datang ke kantor pelayanan terdekat untuk melakukan verifikasi. Terima kasih.</p>
+<div class="wrapper wrapper-content">
+    <div class="row">
+       <div class="widget style1 red-bg">
+            <div class="row vertical-align">
+                <div class="col-xs-1">
+                    <i class="fa fa-bolt fa-2x"></i>
+                </div>
+                <div class="col-xs-11 text-left">
+                    <h4 class="font-bold">Data perizinan yang anda cek tidak ada dalam database kami, silahkan untuk datang ke kantor pelayanan terdekat untuk melakukan verifikasi. Terima kasih.</h4>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-
--- frontend/views/site/valid.php
+++ frontend/views/site/valid.php
@@ -4,6 +4,27 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 $this->context->layout = 'main-no-landing';
 ?>
-<div class="row">
-    <p><?= $validasi; ?></p>
-</div>
+<style>
+.frame-izin {
+    padding:15px;
+    border:2px solid #676a6c;
+    box-shadow:0px 2px 12px 2px #ccc;
+}    
+    
+</style>
+
+<div class="wrapper wrapper-content">
+    <div class="row">
+      
+            <div class="col-lg-12 text-center">
+                <br>
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8 frame-izin">    
+                    <?= $validasi; ?>
+                </div>
+                <div class="col-lg-2"></div>
+            </div>
+             
+    </div>
+ </div>
+
