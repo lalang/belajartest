f98de76d76479d73928c3405fff1b2a2d43b5539
alfarioekaputra <alfarioekapura@gmail.com>
2015-09-29 22:51:38 +0700

    menghapus lay-blank

--- common/config/main.php
+++ common/config/main.php
@@ -17,11 +17,11 @@ return [
             'controllerMap' => [
                 'security' => [
                     'class' => 'dektrium\user\controllers\SecurityController',
-                    'layout' => '@app/views/layouts/lay-blank',
+                    //'layout' => '@app/views/layouts/lay-blank',
                 ],
                 'registration' => [
                     'class' => 'dektrium\user\controllers\RegistrationController',
-                    'layout' => '@app/views/layouts/lay-blank',
+                    //'layout' => '@app/views/layouts/lay-blank',
                 ],
             ],
             'enableUnconfirmedLogin' => true,
@@ -74,7 +74,34 @@ return [
                     'label' => 'Assignment' // change label
                 ],
             ],
-        ]
+        ],
+    		'filemanager' => [
+    				'class' => 'pendalf89\filemanager\Module',
+    				// Upload routes
+    				'routes' => [
+    						// Base absolute path to web directory
+    						'baseUrl' => '',
+    						// Base web directory url
+    						'basePath' => '@frontend/web',
+    						// Path for uploaded files in web directory
+    						'uploadPath' => 'uploads',
+    				],
+    				// Thumbnails info
+    				'thumbs' => [
+    						'small' => [
+    								'name' => 'small',
+    								'size' => [100, 100],
+    						],
+    						'medium' => [
+    								'name' => 'medium',
+    								'size' => [300, 200],
+    						],
+    						'large' => [
+    								'name' => 'large',
+    								'size' => [500, 400],
+    						],
+    				],
+    		],
     ],
     'components' => [
 
