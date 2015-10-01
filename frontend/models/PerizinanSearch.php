ce3e4ffacf08b4bfb96299b08e29008cce91f431
alfarioekaputra <alfarioekapura@gmail.com>
2015-10-01 22:18:39 +0700

    izin siup

--- frontend/models/PerizinanSearch.php
+++ frontend/models/PerizinanSearch.php
@@ -17,7 +17,7 @@ class PerizinanSearch extends Perizinan {
      */
     public function rules() {
         return [
-            [['id', 'parent_id', 'pemohon_id', 'id_groupizin', 'izin_id', 'jumlah_tahap', 'no_urut', 'petugas_daftar_id', 'lokasi_id'], 'integer'],
+            [['id', 'parent_id', 'pemohon_id', 'id_groupizin', 'izin_id', 'jumlah_tahap', 'no_urut', 'petugas_daftar_id', 'lokasi_izin_id'], 'integer'],
             [['tanggal_mohon', 'no_izin', 'berkas_noizin', 'tanggal_izin', 'tanggal_expired', 'status', 'aktif', 'registrasi_urutan', 'nomor_sp_rt_rw', 'tanggal_sp_rt_rw', 'peruntukan', 'nama_perusahaan', 'tanggal_cek_lapangan', 'petugas_cek', 'status_daftar', 'keterangan', 'qr_code', 'tanggal_pertemuan', 'pengambilan_tanggal', 'pengambilan_sesi'], 'safe'],
         ];
     }
@@ -73,7 +73,7 @@ class PerizinanSearch extends Perizinan {
             'tanggal_sp_rt_rw' => $this->tanggal_sp_rt_rw,
             'tanggal_cek_lapangan' => $this->tanggal_cek_lapangan,
             'petugas_daftar_id' => $this->petugas_daftar_id,
-            'lokasi_id' => $this->lokasi_id,
+            'lokasi_izin_id' => $this->lokasi_izin_id,
             'tanggal_pertemuan' => $this->tanggal_pertemuan,
             'pengambilan_tanggal' => $this->pengambilan_tanggal,
             'pengambilan_sesi' => $this->pengambilan_sesi,
