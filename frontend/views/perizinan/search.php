61e96a93a8979a31ab1cf18b61c6342e8dedfdf6
amanrohiman <amanrohiman@192.168.1.102>
2015-10-05 11:15:58 +0700

    permanis form search

--- frontend/views/perizinan/search.php
+++ frontend/views/perizinan/search.php
@@ -12,7 +12,6 @@ use yii\helpers\Url;
 use yii\web\JsExpression;
 use yii\web\View;
 
-
 /* @var $this View */
 /* @var $searchModel PerizinanSearch */
 /* @var $dataProvider ActiveDataProvider */
@@ -55,66 +54,66 @@ $this->registerJs($search);
     <div class="col-sm-1"></div>
     <div class="col-sm-10">
         <?php
-                echo Slider::widget([
-                    'name' => 'current_no',
-                    'value' => 1,
-                    'sliderColor' => Slider::TYPE_INFO,
-                    'handleColor' => Slider::TYPE_DANGER,
-                    'pluginOptions' => [
-                        'min' => 0,
-                        'max' => 6,
-                        'ticks' => [1,2,3,4,5,6],
-                        'ticks_labels' => ['1. Cari Izin','2. Input Formulir','3. Unggah Berkas','4. Atur Jadwal Pengambilan', '5. Pemrosesan Izin', '6. Pengambilan Izin'],
-                        'ticks_snap_bounds' => 50,
-                        'tooltip' => 'always',
-                        'formatter'=>new yii\web\JsExpression("function(val) { 
+        echo Slider::widget([
+            'name' => 'current_no',
+            'value' => 1,
+            'sliderColor' => Slider::TYPE_INFO,
+            'handleColor' => Slider::TYPE_DANGER,
+            'pluginOptions' => [
+                'min' => 0,
+                'max' => 6,
+                'ticks' => [1, 2, 3, 4, 5, 6],
+                'ticks_labels' => ['Cari Izin', 'Input Formulir', 'Unggah Berkas', 'Atur Jadwal Pengambilan', 'Pemrosesan Izin', 'Pengambilan Izin'],
+                'ticks_snap_bounds' => 50,
+                'tooltip' => 'always',
+                'formatter' => new yii\web\JsExpression("function(val) { 
                                 return 'Anda Disini';
                         }")
-                    ],
-                    'options' => ['disabled'=>true,'style' => 'width: 100%']
-                ]);
-            ?>
+            ],
+            'options' => ['disabled' => true, 'style' => 'width: 100%']
+        ]);
+        ?>
     </div>
     <div class="col-sm-1"></div>
 </div>
 <br><br><br><br><br>
-        <div class="row">
-            
-            <div class="col-md-12">
-                
-                <div class="box">
-                    
-                <div class="box-header with-border">
-                    <h3 class="box-title">Perizinan</h3>
-                    <div class="box-tools pull-right">
-                        <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                    </div>
-                </div>
-                <div class="box-body">
-            
-                    <div class="panel-sub-heading">
-                        <div class="callout callout-info">
-                            <p>Silahkan cari perizinan yang anda butuhkan lalu klik tombol Daftar untuk membuat permohonan</p>
-                        </div>
-                    </div><!-- /.panel-sub-heading -->
-                    <br>
-                    <?php
-                    $form = ActiveForm::begin([
-                                'method' => 'post',
-                                'layout' => 'horizontal',
-                    ]);
-                    ?>
+<div class="row">
 
-                    <?=
-                    $form->field($model, 'izin')->widget(Select2::classname(), [
-                        'data' => ArrayHelper::map(Izin::find()->orderBy('id')->asArray()->all(), 'id', 'nama'),
-                        'options' => [
-                            'id' => 'izin-id',
-                            'placeholder' => Yii::t('app', 'Ketik atau pilih nama izin atau bidang'),
-                            'class' => 'col-md-6',
-                            'onchange'=>"
+    <div class="col-md-12">
+
+        <div class="box">
+
+            <div class="box-header with-border">
+                <h3 class="box-title">Perizinan</h3>
+                <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                </div>
+            </div>
+            <div class="box-body">
+
+                <div class="panel-sub-heading">
+                    <div class="callout callout-info">
+                        <p>Silahkan cari perizinan yang anda butuhkan lalu klik tombol Daftar untuk membuat permohonan</p>
+                    </div>
+                </div><!-- /.panel-sub-heading -->
+                <br>
+                <?php
+                $form = ActiveForm::begin([
+                            'method' => 'post',
+                            'layout' => 'horizontal',
+                ]);
+                ?>
+
+                <?=
+                $form->field($model, 'izin')->widget(Select2::classname(), [
+                    'data' => ArrayHelper::map(Izin::find()->orderBy('id')->asArray()->all(), 'id', 'nama'),
+                    'options' => [
+                        'id' => 'izin-id',
+                        'placeholder' => Yii::t('app', 'Ketik atau pilih nama izin atau bidang'),
+                        'class' => 'col-md-6',
+                        'onchange' => "
                                 $.ajax({
-                                    url: '".Url::to(['izin-label'])."',
+                                    url: '" . Url::to(['izin-label']) . "',
                                     type: 'GET',
                                     data:{izin:$('#izin-id').val() },
                                     dataType: 'html',
@@ -125,41 +124,53 @@ $this->registerJs($search);
                                     }
                                 });
                             "
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => false,
+                        'minimumInputLength' => 3,
+                        'ajax' => [
+                            'url' => Url::to(['izin-search']),
+                            'dataType' => 'json',
+                            'data' => new JsExpression('function(params) { return {search:params.term}; }'),
+                            'results' => new JsExpression('function(data,page) { return {results:data.results}; }'),
                         ],
-                        
-                        'pluginOptions' => [
-                            'allowClear' => false,
-                            'minimumInputLength' => 3,
-                            'ajax' => [
-                                'url' => Url::to(['izin-search']),
-                                'dataType' => 'json',
-                                'data' => new JsExpression('function(params) { return {search:params.term}; }'),
-                                'results' => new JsExpression('function(data,page) { return {results:data.results}; }'),
-                            ],
-                        ],
-                    ]);
-                    ?>
+                    ],
+                ]);
+                ?>
 
-                    <?= $form->field($model, 'bidang_izin')->textInput(['readonly' => true])  ?>
-                    <div id="ket-lb"></div>
-                    <div id="tipe" style="display:none">
-                        <?= $form->field($model, 'tipe')->textInput(['value' => Yii::$app->user->identity->profile->tipe, 'readonly' => true]) ?>
-                        <div class="col-sm-3">&nbsp</div>
-                        <div class="col-sm-9">* Jika anda ingin melakukan permohonan izin sebagai perusahaan silahkan login sebagai Perusahaan</div>
-                        <?= $form->field($model, 'status')->dropDownList([ 'Perubahan' => 'Perubahan', 'Perpanjangan' => 'Perpanjangan', 'Baru' => 'Baru'], ['prompt' => 'Pilih Status..', 'id' => 'status-id']) ?>
-                    </div>
-                    <div id="daftar" style="display:none">
-                        <div class="form-group text-center">
-                            <?= Html::submitButton(Yii::t('app', 'Buat Permohonan'), ['class' => 'btn btn-primary']) ?>
+                <?= $form->field($model, 'bidang_izin')->textInput(['readonly' => true]) ?>
+                <div id="ket-lb"></div>
+                <div id="tipe" style="display:none">
+                    <?= $form->field($model, 'tipe')->textInput(['value' => Yii::$app->user->identity->profile->tipe, 'readonly' => true]) ?>
+                    <div class="form-group">
+                        <label class="control-label col-sm-3"></label>
+                        <div class="col-sm-6">
+                            <div class="alert alert-warning alert-dismissible">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                <h4><i class="icon fa fa-warning"></i> Peringatan!</h4>
+                                <?php
+                                if (Yii::$app->user->identity->profile->tipe == 'Perorangan') {
+                                    echo 'Jika anda ingin melakukan permohonan izin sebagai perusahaan silahkan login sebagai akun perusahaan';
+                                } else {
+                                    echo 'Jika anda ingin melakukan permohonan izin sebagai perorangan silahkan login sebagai akun perorangan';
+                                }
+                                ?>
+                            </div>
                         </div>
                     </div>
-
-
-                    <?php ActiveForm::end(); ?>
+<?= $form->field($model, 'status')->dropDownList([ 'Perubahan' => 'Perubahan', 'Perpanjangan' => 'Perpanjangan', 'Baru' => 'Baru'], ['prompt' => 'Pilih Status..', 'id' => 'status-id']) ?>
                 </div>
-                <div class="box-footer"></div>
-            </div>
-            </div>
-        </div><!-- /.row -->
+                <div id="daftar" style="display:none">
+                    <div class="form-group text-center">
+<?= Html::submitButton(Yii::t('app', 'Buat Permohonan'), ['class' => 'btn btn-primary']) ?>
+                    </div>
+                </div>
 
-    
+
+<?php ActiveForm::end(); ?>
+            </div>
+            <div class="box-footer"></div>
+        </div>
+    </div>
+</div><!-- /.row -->
+
