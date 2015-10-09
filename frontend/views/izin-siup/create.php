eb7ac259b25a4fb740f4b53fd7c20ffcc8e74bfd
amanrohiman <amanrohiman@192.168.1.103>
2015-10-09 10:13:19 +0700

    pemindahan progress

--- frontend/views/izin-siup/create.php
+++ frontend/views/izin-siup/create.php
@@ -18,8 +18,27 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="col-sm-1"></div>
 </div>
                 
-        <?=
-        $this->render('_form', [
-            'model' => $model,
-        ])
-        ?>
+        <br>
+<div style="text-align: center">
+    <?=
+    Slider::widget([
+        'name' => 'current_no',
+        'value' => 2,
+        'sliderColor' => Slider::TYPE_GREY,
+        'handleColor' => Slider::TYPE_DANGER,
+        'pluginOptions' => [
+//            'min' => 1,
+//            'max' => $model->jumlah_tahap,
+            'ticks' => [1,2,3,4,5,6,7],
+            'ticks_labels' => ['Cari Izin','Input Formulir','Unggah Berkas','Preview SK','Jadwal Pengambilan', 'Pemrosesan Izin', 'Pengambilan Izin'],
+            'ticks_snap_bounds' => 50,
+            'tooltip' => 'always',
+            'formatter'=>new yii\web\JsExpression("function(val) {
+               return 'Anda Disini';
+            }")
+        ],
+        'options' => ['disabled' => true, 'style' => 'width: 80%']
+    ]);
+    ?>
+</div>
+<br>
