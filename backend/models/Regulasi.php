0d27e44a93579d7a68bdb5f54badcfc9cfa1d4e2
ErwinLim <ErwinLim@ErwinLim-PC>
2015-10-30 12:07:48 +0700

    Update CRUD Tabel NoIzin dan NoPenolakan dan Merapikan Form NoIzin NoPenolakan dan update link ke left menu admin

---
+++ backend/models/Regulasi.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use \backend\models\base\Regulasi as BaseRegulasi;
+
+/**
+ * This is the model class for table "regulasi".
+ */
+class Regulasi extends BaseRegulasi
+{
+    
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['publish'], 'string'],
+			[['urutan'], 'integer'],
+            [['nama', 'nama_en'], 'string', 'max' => 225]
+        ];
+    }
+	
+}
---
+++ backend/views/regulasi/_formDownload.php
@@ -0,0 +1,82 @@
+<?php
+use kartik\grid\GridView;
+use kartik\builder\TabularForm;
+use yii\data\ArrayDataProvider;
+use yii\helpers\Html;
+use yii\widgets\Pjax;
+
+Pjax::begin();
+$dataProvider = new ArrayDataProvider([
+    'allModels' => $row,
+]);
+echo TabularForm::widget([
+    'dataProvider' => $dataProvider,
+    'formName' => 'Download',
+    'checkboxColumn' => false,
+    'actionColumn' => false,
+    'attributeDefaults' => [
+        'type' => TabularForm::INPUT_TEXT,
+    ],
+    'attributes' => [
+        "id" => ['type' => TabularForm::INPUT_HIDDEN, 'columnOptions'=>['hidden'=>true]],
+        'id_regulasi' => [
+            'label' => 'Regulasi',
+            'type' => TabularForm::INPUT_WIDGET,
+            'widgetClass' => \kartik\widgets\Select2::className(),
+            'options' => [
+                'data' => \yii\helpers\ArrayHelper::map(\backend\models\Regulasi::find()->orderBy('id')->asArray()->all(), 'id', 'id'),
+                'options' => ['placeholder' => 'Choose Regulasi'],
+            ],
+            'columnOptions' => ['width' => '200px']
+        ],
+        'judul' => ['type' => TabularForm::INPUT_TEXT],
+        'judul_eng' => ['type' => TabularForm::INPUT_TEXT],
+        'deskripsi' => ['type' => TabularForm::INPUT_TEXTAREA],
+        'deskripsi_eng' => ['type' => TabularForm::INPUT_TEXTAREA],
+        'nama_file' => ['type' => TabularForm::INPUT_TEXT],
+        'jenis_file' => ['type' => TabularForm::INPUT_DROPDOWN_LIST,
+                    'options' => [
+                        'items' => [ 'Dok' => 'Dok', 'SW' => 'SW', 'Drv' => 'Drv', ],
+                        'columnOptions => ['width' => '185px'],
+                        'options' => ['placeholder' => 'Choose Jenis File'],
+                    ]
+        ],
+        'tanggal' => ['type' => TabularForm::INPUT_WIDGET,
+        'widgetClass' => \kartik\widgets\DatePicker::classname(),
+        'options' => [
+            'options' => ['placeholder' => 'Choose Tanggal'],
+            'type' => \kartik\widgets\DatePicker::TYPE_COMPONENT_APPEND,
+            'pluginOptions' => [
+                'autoclose' => true,
+                'format' => 'dd-M-yyyy'
+            ]
+        ]
+],
+        'diunduh' => ['type' => TabularForm::INPUT_TEXT],
+        'publish' => ['type' => TabularForm::INPUT_DROPDOWN_LIST,
+                    'options' => [
+                        'items' => [ 'Y' => 'Y', 'N' => 'N', ],
+                        'columnOptions => ['width' => '185px'],
+                        'options' => ['placeholder' => 'Choose Publish'],
+                    ]
+        ],
+        'del' => [
+            'type' => TabularForm::INPUT_STATIC,
+            'label' => '',
+            'value' => function($model, $key) {
+                return Html::a('<i class="glyphicon glyphicon-trash"></i>', '#', ['title' =>  'Delete', 'onClick' => 'delRowDownload(' . $key . '); return false;', 'id' => 'download-del-btn']);
+            },
+        ],
+    ],
+    'gridSettings' => [
+        'panel' => [
+            'heading' => '<h3 class="panel-title"><i class="glyphicon glyphicon-book"></i> ' . 'Download' . '  </h3>',
+            'type' => GridView::TYPE_INFO,
+            'before' => false,
+            'footer' => false,
+            'after' => Html::button('<i class="glyphicon glyphicon-plus"></i>' . 'Add Row', ['type' => 'button', 'class' => 'btn btn-success kv-batch-create', 'onClick' => 'addRowDownload()']),
+        ]
+    ]
+]);
+Pjax::end();
+?>
