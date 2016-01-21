3b4b0b7105ee74d71d6b6d70a61ff95ec04e9ccd
ErwinLim <ErwinLim@ErwinLim-PC>
2016-01-21 11:17:35 +0700

    Fix tombol lihat yang ada di petugas

--- backend/controllers/PerizinanController.php
+++ backend/controllers/PerizinanController.php
@@ -94,15 +94,24 @@ class PerizinanController extends Controller {
 //        if(in_array($model->izin_id, array(619,621,622,626))) {
 //            $model_izin= IzinSiup::findOne($model->referrer_id);
 //        }
+        
+        if($model->izin->type=='TDG'){ 
+            $model_izin = \backend\models\IzinTdg::findOne($model->referrer_id);
+        } elseif($model->izin->type=='PM1'){
+            $model_izin = \backend\models\IzinPm1::findOne($model->referrer_id);
+            
+        } else{
+            $model_izin = \backend\models\IzinSiup::findOne($model->referrer_id);
+        }
      
          $izin = Izin::findOne($model->izin_id);
          switch ($izin->action) {
-         case 'izin-siup':
-         $model_izin = IzinSiup::findOne($model->referrer_id);
-         break;
-         case 'tdp':
-         $model_izin = IzinSiup::findOne($model->referrer_id);
-         break;
+            case 'izin-siup':
+                $model_izin = IzinSiup::findOne($model->referrer_id);
+                break;
+            case 'tdp':
+                $model_izin = IzinSiup::findOne($model->referrer_id);
+                break;
          }
         return $this->renderAjax('_lihat',[ 
            'model' => $model_izin,]);
