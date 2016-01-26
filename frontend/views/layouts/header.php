186b65de0e70b170d9add81cfcfc2039c66877e1
fazkha <mr.eko.handriyanto@gmail.com>
2016-01-26 19:31:58 +0700

    Perbaikan css

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -5,14 +5,10 @@ use yii\helpers\Html;
 /* @var $this \yii\web\View */
 /* @var $content string */
 ?>
-<style>
-    .sidebar-menu>li>a>.fa {font-size:122%;width:22px;}
-    
-</style>
 
 <header class="main-header">
 
-    <img class="main-header logo" src="<?= Yii::getAlias('@web') ?>/images/logo-dki-small.png" style="margin-left: -11px; background-color:#00a65a;border:none; width:auto;height:50px;margin-right:-77px">
+    <img class="main-header logo" src="<?= Yii::getAlias('@web') ?>/images/logo-dki-small.png" style="margin-left: -11px; border:none; width:auto;height:50px;margin-right:-77px">
     <!--<?= Html::a('<span class="logo-mini">PTSP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>-->
     <?= Html::a('PTSP DKI', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
