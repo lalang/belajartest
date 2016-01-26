02f7fe93c4a18ec40c894697898ed5dc04df0c3a
fazkha <mr.eko.handriyanto@gmail.com>
2016-01-26 20:16:30 +0700

    Perbaikan css

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -6,10 +6,6 @@ use yii\helpers\Html;
 
 
 if (Yii::$app->controller->action->id === 'login') { 
-/**
- * Do not use this code in your template. Remove it. 
- * Instead, use the code  $this->layout = '//main-login'; in your controller.
- */
     echo $this->render(
         'main-login',
         ['content' => $content]
