aed7163934193389cf24ce3398a6f5b966fdd5c2
ErwinLim <ErwinLim@ErwinLim-PC>
2016-02-02 13:52:56 +0700

    CRUD Table Package Backend Admin

---
+++ backend/models/PackageSearch.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\Package;
+
+/**
+ * backend\models\PackageSearch represents the model behind the search form about `backend\models\Package`.
+ */
+ class PackageSearch extends Package
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'izin_id', 'paket_izin_id'], 'integer'],
+            [['status'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params, $id)
+    {
+        $query = Package::find()->where(['izin_id'=>$id])->orderBy('id asc');
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'izin_id' => $this->izin_id,
+            'paket_izin_id' => $this->paket_izin_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'status', $this->status]);
+
+        return $dataProvider;
+    }
+}
---
+++ backend/views/package/_form.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Package */
+/* @var $form yii\widgets\ActiveForm */
+
+?>
+
+<div class="package-form">
+
+    <?= Html::a(Yii::t('app', '<i class="fa fa-angle-double-left"></i> Kembali'), ['index', 'id'=>$_SESSION['id_induk']], ['class' => 'btn btn-warning']) ?>
+    
+    <?php $form = ActiveForm::begin(); ?>
+    
+    <?= $form->errorSummary($model); ?>
+
+    <?= $form->field($model, 'id', ['template' => '{input}'])->textInput(['style' => 'display:none']); ?>
+    
+    <?= $form->field($model, 'izin_id', ['template' => '{input}'])->textInput(['value'=>$_SESSION['id_induk'], 'style' => 'display:none']); ?>
+    
+    <?php
+
+        $ActifRecord = backend\models\Package::find()->where(['izin_id'=>$_SESSION['id_induk']])->select('paket_izin_id');
+        $typeParent = backend\models\Izin::findOne($_SESSION['id_induk'])->tipe;
+        $query = \backend\models\Izin::find()
+                ->where(['tipe'=>$typeParent])
+                ->andWhere(['not in','id',$ActifRecord])
+                ->orderBy('id')->asArray()->all();
+    ?>
+    
+    <?= $form->field($model, 'paket_izin_id')->widget(\kartik\widgets\Select2::classname(), [
+        'data' => \yii\helpers\ArrayHelper::map($query, 'id', 'nama'),
+        'options' => ['placeholder' => Yii::t('app', 'Choose Izin')],
+        'pluginOptions' => [
+            'allowClear' => true
+        ],
+    ]) ?>
+
+    <?= $form->field($model, 'status')->dropDownList([ 'Y' => 'Y', 'N' => 'N', ]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
