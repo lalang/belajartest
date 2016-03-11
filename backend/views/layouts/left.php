924ccfe06e6f4462b113df5b7ec9c0e0c263ad91
ErwinLim <ErwinLim@ErwinLim-PC>
2016-03-11 14:28:27 +0700

    kabupaten tabular tdp

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -37,6 +37,7 @@
                                     ['label' => 'Verifikasi Berkas Tolak', 'icon' => 'fa fa-times', 'url' => ['/perizinan/index', 'status' => 'verifikasi-tolak']],
                                     ['label' => 'Konfimasi Pemohon', 'icon' => 'fa fa-user', 'url' => ['/perizinan/confirm-pemohon']],
                                     ['label' => '----------------------------------------------'],
+                                    ['label' => 'Daftar Pemohon Offline', 'icon' => 'fa fa-pencil', 'url' => ['/perizinan/search']],
                                     ['label' => 'Daftar Perizinan Offline', 'icon' => 'fa fa-pencil', 'url' => ['/perizinan/search']],
                                     ['label' => 'Perizinan Offline Pending', 'icon' => 'fa fa-pencil-square-o', 'url' => ['/perizinan/pending']],
                                     ['label' => 'Lacak Status Permohonan', 'icon' => 'fa fa-search', 'url' => ['/perizinan/lacak']],
