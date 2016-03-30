4c3df94168394c8d97afe3000ac103cd617e87c1
Samuel <Samuel@DESKTOP-H30MI7V>
2016-03-30 14:27:44 +0700

    lokasi di buka

--- backend/models/LokasiSearch.php
+++ backend/models/LokasiSearch.php
@@ -43,7 +43,9 @@ use backend\models\Lokasi;
     public function search($params)
     {
         /* Eko 261115 - order by disesuaikan dengan kebutuhan user */
-        $query = Lokasi::find()->where(['propinsi' => 31])->orderBy('kode asc');
+        $query = Lokasi::find()
+//                ->where(['propinsi' => 31])
+                ->orderBy('kode asc');
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
