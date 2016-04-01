4c631ff7a909115481522eaaa6408859221c91e3
Lalang <lalanggumirang@yahoo.com>
2016-04-01 13:04:44 +0700

    Golongan Gudang

---
+++ backend/models/GolonganGudang.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use \backend\models\base\GolonganGudang as BaseGolonganGudang;
+
+/**
+ * This is the model class for table "golongan_gudang".
+ */
+class GolonganGudang extends BaseGolonganGudang
+{
+    
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['nama'], 'required'],
+            [['id'], 'integer'],
+            [['publish'], 'string'],
+            [['nama', 'luas', 'kapasitas_penyimpanan', 'bentuk'], 'string', 'max' => 50]
+        ];
+    }
+	
+}
---
+++ backend/views/golongan-gudang/_search.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\GolonganGudangSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="golongan-gudang-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'nama') ?>
+
+    <?= $form->field($model, 'luas') ?>
+
+    <?= $form->field($model, 'kapasitas_penyimpanan') ?>
+
+    <?= $form->field($model, 'bentuk') ?>
+
+    <?php // echo $form->field($model, 'publish') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
