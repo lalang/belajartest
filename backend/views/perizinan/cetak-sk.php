6c9032bd9f0aa211b9fcac5e8bf2cd2cc19acac3
ErwinLim <ErwinLim@ErwinLim-PC>
2016-04-28 16:44:06 +0700

    Fix View BAPL dan Param SKDP

--- backend/views/perizinan/cetak-sk.php
+++ backend/views/perizinan/cetak-sk.php
@@ -107,7 +107,7 @@ $this->params['breadcrumbs'][] = ['label' => 'Cetak SK'];
                     <?php
                     if($model2->file_bapl){
                         
-                        echo Html::a('<i class="fa fa-eye"></i> ' . Yii::t('app', 'View BAPL'), [Yii::getAlias('@test') . '/images/documents/bapl/' . $model2->izin_id . '/' . $model2->file_bapl], [
+                        echo Html::a('<i class="fa fa-eye"></i> ' . Yii::t('app', 'View BAPL'), ['/images/documents/bapl/' . $model2->izin_id . '/' . $model2->file_bapl], [
                             'target' => '_blank',
                             'data-toggle' => 'tooltip',
                             'class' => 'btn btn-info',
