285f9d1597990d6acb4e328b422dac9cba315ac1
ErwinLim <ErwinLim@ErwinLim-PC>
2016-05-16 17:45:50 +0700

    Split DB di tabel transaksi

--- backend/models/base/IzinSiup.php
+++ backend/models/base/IzinSiup.php
@@ -200,4 +200,10 @@ class IzinSiup extends \yii\db\ActiveRecord
     {
         return new \backend\models\IzinSiupQuery(get_called_class());
     }
+    
+    public static function getDb()
+    {
+        // use the "db2" application component
+        return \Yii::$app->dbTrans;  
+    }
 }
--- backend/models/base/IzinSiupKbli.php
+++ backend/models/base/IzinSiupKbli.php
@@ -64,4 +64,10 @@ class IzinSiupKbli extends \yii\db\ActiveRecord
     {
         return new \backend\models\IzinSiupKbliQuery(get_called_class());
     }
+    
+    public static function getDb()
+    {
+        // use the "db2" application component
+        return \Yii::$app->dbTrans;  
+    }
 }
