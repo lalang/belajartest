b0946b7561d16d7e7740ea5ea3d9669c23b058ef
Lalang <lalanggumirang@yahoo.com>
2016-06-22 13:49:09 +0700

    Kesehatan

--- backend/web/js/wizard_form_laporan.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('#form-laporan').on('submit', function() {
-		if(!$('#laporan-id_laporan').val()) {
-			alert('laporan perusahaan tidak boleh kosong');
-			$('#laporan-id_laporan').focus();
-			return false;
-		}
-	});
-});
