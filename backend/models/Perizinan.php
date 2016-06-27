b9ece1d8b250ef7eca8455491d28d52116870e96
Samuel <Samuel@DESKTOP-H30MI7V>
2016-06-27 10:16:29 +0700

    kesehatan

--- backend/models/Perizinan.php
+++ backend/models/Perizinan.php
@@ -365,8 +365,9 @@ class Perizinan extends BasePerizinan {
     }
 
     public static function getNew() {
-        return Perizinan::find()->joinWith('izin')
-                        ->Where('status = "daftar" '
+        return Perizinan::find()->joinWith(['izin'])
+                        ->Where('perizinan.status in ("daftar","proses") '
+                                . 'perizinan_proses.action = "registrasi"'
                                 . 'AND tanggal_mohon >= "2016-01-01" '
                                 . 'AND izin.wewenang_id=' . Yii::$app->user->identity->wewenang_id . ' '
                                 . 'AND perizinan.lokasi_izin_id = ' . Yii::$app->user->identity->lokasi_id. ' '
