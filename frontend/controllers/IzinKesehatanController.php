3e51225508fd2dadd4a817eb72dbd95889fb595b
Lalang <lalanggumirang@yahoo.com>
2016-06-27 09:19:47 +0700

    Kesehatan

--- frontend/controllers/IzinKesehatanController.php
+++ frontend/controllers/IzinKesehatanController.php
@@ -11,13 +11,13 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\helpers\Json;
+
 /**
  * IzinKesehatanController implements the CRUD actions for IzinKesehatan model.
  */
-class IzinKesehatanController extends Controller
-{
-    public function behaviors()
-    {
+class IzinKesehatanController extends Controller {
+
+    public function behaviors() {
         return [
             'verbs' => [
                 'class' => VerbFilter::className(),
@@ -32,14 +32,13 @@ class IzinKesehatanController extends Controller
      * Lists all IzinKesehatan models.
      * @return mixed
      */
-    public function actionIndex()
-    {
+    public function actionIndex() {
         $searchModel = new IzinKesehatanSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
+                    'searchModel' => $searchModel,
+                    'dataProvider' => $dataProvider,
         ]);
     }
 
@@ -48,8 +47,7 @@ class IzinKesehatanController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionView($id)
-    {
+    public function actionView($id) {
         $model = $this->findModel($id);
         $providerIzinKesehatanJadwal = new \yii\data\ArrayDataProvider([
             'allModels' => $model->izinKesehatanJadwals,
@@ -61,10 +59,10 @@ class IzinKesehatanController extends Controller
             'allModels' => $model->izinKesehatanJadwalSatus,
         ]);
         return $this->render('view', [
-            'model' => $this->findModel($id),
-            'providerIzinKesehatanJadwal' => $providerIzinKesehatanJadwal,
-            'providerIzinKesehatanJadwalDua' => $providerIzinKesehatanJadwalDua,
-            'providerIzinKesehatanJadwalSatu' => $providerIzinKesehatanJadwalSatu,
+                    'model' => $this->findModel($id),
+                    'providerIzinKesehatanJadwal' => $providerIzinKesehatanJadwal,
+                    'providerIzinKesehatanJadwalDua' => $providerIzinKesehatanJadwalDua,
+                    'providerIzinKesehatanJadwalSatu' => $providerIzinKesehatanJadwalSatu,
         ]);
     }
 
@@ -73,21 +71,20 @@ class IzinKesehatanController extends Controller
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
-    public function actionCreate($id)
-    {
+    public function actionCreate($id) {
         $model = new IzinKesehatan();
-        $izin = Izin::findOne($id);		
+        $izin = Izin::findOne($id);
         $model->izin_id = $izin->id;
         $model->status_id = $izin->status_id;
         $model->user_id = Yii::$app->user->id;
         $model->tipe = $izin->tipe;
-		$model->kode = $izin->kode;
+        $model->nama_izin = $izin->nama;
 
-        if($model->tipe == "Perorangan") {
-             if(Yii::$app->user->identity->status == 'DKI'){
-                $arrAlamat = explode(" RW ",Yii::$app->user->identity->profile->alamat);
+        if ($model->tipe == "Perorangan") {
+            if (Yii::$app->user->identity->status == 'DKI') {
+                $arrAlamat = explode(" RW ", Yii::$app->user->identity->profile->alamat);
                 $RW = $arrAlamat[1];
-                $arrAlamat = explode(" RT ",$arrAlamat[0]);
+                $arrAlamat = explode(" RT ", $arrAlamat[0]);
                 $RT = $arrAlamat[1];
                 $model->alamat = $arrAlamat[0];
                 $model->rw = $RW;
@@ -96,7 +93,6 @@ class IzinKesehatanController extends Controller
                 $model->wilayah_id = Yii::$app->user->identity->kdwil;
                 $model->kecamatan_id = Yii::$app->user->identity->kdkec;
                 $model->kelurahan_id = Yii::$app->user->identity->kdkel;
-                
             } else {
                 $model->alamat = Yii::$app->user->identity->profile->alamat;
             }
@@ -108,11 +104,11 @@ class IzinKesehatanController extends Controller
             $model->email = Yii::$app->user->identity->email;
         }
         if ($model->loadAll(Yii::$app->request->post()) && $model->saveAll()) {
-            
-            return $this->redirect(['/perizinan/upload', 'id'=>$model->perizinan_id, 'ref'=>$model->id]);
+
+            return $this->redirect(['/perizinan/upload', 'id' => $model->perizinan_id, 'ref' => $model->id]);
         } else {
             return $this->render('create', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
@@ -123,17 +119,16 @@ class IzinKesehatanController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionUpdate($id)
-    {
+    public function actionUpdate($id) {
         $model = $this->findModel($id);
 
         if ($model->loadAll(Yii::$app->request->post()) && $model->saveAll()) {
             Perizinan::updateAll(['update_by' => Yii::$app->user->identity->id, 'update_date' => date("Y-m-d")], ['id' => $model->perizinan_id]);
-            
-            return $this->redirect(['/perizinan/upload', 'id'=>$model->perizinan_id, 'ref'=>$model->id]);
+
+            return $this->redirect(['/perizinan/upload', 'id' => $model->perizinan_id, 'ref' => $model->id]);
         } else {
             return $this->render('update', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
@@ -144,13 +139,12 @@ class IzinKesehatanController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionDelete($id)
-    {
+    public function actionDelete($id) {
         $this->findModel($id)->deleteWithRelated();
 
         return $this->redirect(['index']);
     }
-    
+
     /**
      * Finds the IzinKesehatan model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
@@ -158,14 +152,14 @@ class IzinKesehatanController extends Controller
      * @return IzinKesehatan the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($id)
-    {
+    protected function findModel($id) {
         if (($model = IzinKesehatan::findOne($id)) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
         }
     }
+
     public function actionSubkot() {
         $out = [];
         if (isset($_POST['depdrop_parents'])) {
@@ -174,18 +168,18 @@ class IzinKesehatanController extends Controller
                 $kot_id = $parents[0];
                 $out = \backend\models\Lokasi::getAllKotOptions($kot_id);
                 if (!empty($_POST['depdrop_params'])) {
-                 $params = $_POST['depdrop_params'];
-                 $selected = $params[0];
-                 }  else {
-                     $selected = '';
-                 }
+                    $params = $_POST['depdrop_params'];
+                    $selected = $params[0];
+                } else {
+                    $selected = '';
+                }
                 echo Json::encode(['output' => $out, 'selected' => $selected]);
                 return;
             }
         }
         echo Json::encode(['output' => '', 'selected' => '']);
     }
-    
+
     public function actionSubkec() {
         $out = [];
         if (isset($_POST['depdrop_parents'])) {
@@ -195,18 +189,18 @@ class IzinKesehatanController extends Controller
             if ($cat_id != null) {
                 $data = \backend\models\Lokasi::getAllKecOptions($cat_id, $subcat_id);
                 if (!empty($_POST['depdrop_params'])) {
-                 $params = $_POST['depdrop_params'];
-                 $selected = $params[0];
-                 }  else {
-                     $selected = '';
-                 }
+                    $params = $_POST['depdrop_params'];
+                    $selected = $params[0];
+                } else {
+                    $selected = '';
+                }
                 echo Json::encode(['output' => $data, 'selected' => $selected]);
                 return;
             }
         }
         echo Json::encode(['output' => '', 'selected' => '']);
     }
-    
+
     public function actionSubkel() {
         $out = [];
         if (isset($_POST['depdrop_parents'])) {
@@ -217,18 +211,18 @@ class IzinKesehatanController extends Controller
             if ($prov_id != null) {
                 $data = \backend\models\Lokasi::getAllKelOptions($prov_id, $subkot_id, $subkec_id);
                 if (!empty($_POST['depdrop_params'])) {
-                 $params = $_POST['depdrop_params'];
-                 $selected = $params[0];
-                 }  else {
-                     $selected = '';
-                 }
+                    $params = $_POST['depdrop_params'];
+                    $selected = $params[0];
+                } else {
+                    $selected = '';
+                }
                 echo Json::encode(['output' => $data, 'selected' => $selected]);
                 return;
             }
         }
         echo Json::encode(['output' => '', 'selected' => '']);
     }
-    
+
     public function actionSubcat() {
         $out = [];
         if (isset($_POST['depdrop_parents'])) {
@@ -237,17 +231,18 @@ class IzinKesehatanController extends Controller
                 $cat_id = $parents[0];
                 $out = \backend\models\Lokasi::getKecOptions($cat_id);
                 if (!empty($_POST['depdrop_params'])) {
-                 $params = $_POST['depdrop_params'];
-                 $selected = $params[0];
-                 }  else {
-                     $selected = '';
-                 }
+                    $params = $_POST['depdrop_params'];
+                    $selected = $params[0];
+                } else {
+                    $selected = '';
+                }
                 echo Json::encode(['output' => $out, 'selected' => $selected]);
                 return;
             }
         }
         echo Json::encode(['output' => '', 'selected' => '']);
     }
+
     public function actionProd() {
         $out = [];
         if (isset($_POST['depdrop_parents'])) {
@@ -257,74 +252,73 @@ class IzinKesehatanController extends Controller
             if ($cat_id != null) {
                 $data = \backend\models\Lokasi::getLurahOptions($cat_id, $subcat_id);
                 if (!empty($_POST['depdrop_params'])) {
-                 $params = $_POST['depdrop_params'];
-                 $selected = $params[0];
-                 }  else {
-                     $selected = '';
-                 }
+                    $params = $_POST['depdrop_params'];
+                    $selected = $params[0];
+                } else {
+                    $selected = '';
+                }
                 echo Json::encode(['output' => $data, 'selected' => $selected]);
                 return;
             }
         }
         echo Json::encode(['output' => '', 'selected' => '']);
     }
+
     /**
-    * Action to load a tabular form grid
-    * for IzinKesehatanJadwal
-    * @author Yohanes Candrajaya <moo.tensai@gmail.com>
-    * @author Jiwantoro Ndaru <jiwanndaru@gmail.com>
-    *
-    * @return mixed
-    */
-    public function actionAddIzinKesehatanJadwal()
-    {
+     * Action to load a tabular form grid
+     * for IzinKesehatanJadwal
+     * @author Yohanes Candrajaya <moo.tensai@gmail.com>
+     * @author Jiwantoro Ndaru <jiwanndaru@gmail.com>
+     *
+     * @return mixed
+     */
+    public function actionAddIzinKesehatanJadwal() {
         if (Yii::$app->request->isAjax) {
             $row = Yii::$app->request->post('IzinKesehatanJadwal');
-            if((Yii::$app->request->post('isNewRecord') && Yii::$app->request->post('action') == 'load' && empty($row)) || Yii::$app->request->post('action') == 'add')
+            if ((Yii::$app->request->post('isNewRecord') && Yii::$app->request->post('action') == 'load' && empty($row)) || Yii::$app->request->post('action') == 'add')
                 $row[] = [];
             return $this->renderAjax('_formIzinKesehatanJadwal', ['row' => $row]);
         } else {
             throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
         }
     }
-    
+
     /**
-    * Action to load a tabular form grid
-    * for IzinKesehatanJadwalDua
-    * @author Yohanes Candrajaya <moo.tensai@gmail.com>
-    * @author Jiwantoro Ndaru <jiwanndaru@gmail.com>
-    *
-    * @return mixed
-    */
-    public function actionAddIzinKesehatanJadwalDua()
-    {
+     * Action to load a tabular form grid
+     * for IzinKesehatanJadwalDua
+     * @author Yohanes Candrajaya <moo.tensai@gmail.com>
+     * @author Jiwantoro Ndaru <jiwanndaru@gmail.com>
+     *
+     * @return mixed
+     */
+    public function actionAddIzinKesehatanJadwalDua() {
         if (Yii::$app->request->isAjax) {
             $row = Yii::$app->request->post('IzinKesehatanJadwalDua');
-            if((Yii::$app->request->post('isNewRecord') && Yii::$app->request->post('action') == 'load' && empty($row)) || Yii::$app->request->post('action') == 'add')
+            if ((Yii::$app->request->post('isNewRecord') && Yii::$app->request->post('action') == 'load' && empty($row)) || Yii::$app->request->post('action') == 'add')
                 $row[] = [];
             return $this->renderAjax('_formIzinKesehatanJadwalDua', ['row' => $row]);
         } else {
             throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
         }
     }
-    
+
     /**
-    * Action to load a tabular form grid
-    * for IzinKesehatanJadwalSatu
-    * @author Yohanes Candrajaya <moo.tensai@gmail.com>
-    * @author Jiwantoro Ndaru <jiwanndaru@gmail.com>
-    *
-    * @return mixed
-    */
-    public function actionAddIzinKesehatanJadwalSatu()
-    {
+     * Action to load a tabular form grid
+     * for IzinKesehatanJadwalSatu
+     * @author Yohanes Candrajaya <moo.tensai@gmail.com>
+     * @author Jiwantoro Ndaru <jiwanndaru@gmail.com>
+     *
+     * @return mixed
+     */
+    public function actionAddIzinKesehatanJadwalSatu() {
         if (Yii::$app->request->isAjax) {
             $row = Yii::$app->request->post('IzinKesehatanJadwalSatu');
-            if((Yii::$app->request->post('isNewRecord') && Yii::$app->request->post('action') == 'load' && empty($row)) || Yii::$app->request->post('action') == 'add')
+            if ((Yii::$app->request->post('isNewRecord') && Yii::$app->request->post('action') == 'load' && empty($row)) || Yii::$app->request->post('action') == 'add')
                 $row[] = [];
             return $this->renderAjax('_formIzinKesehatanJadwalSatu', ['row' => $row]);
         } else {
             throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
         }
     }
+
 }
