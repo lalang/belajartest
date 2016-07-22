83b85450ca1fc1187a69c9e80c4bae3a4d5e09e0
Lalang <lalanggumirang@yahoo.com>
2016-07-22 20:55:39 +0700

    Siup Admin

--- frontend/controllers/IzinKesehatanController.php
+++ frontend/controllers/IzinKesehatanController.php
@@ -398,7 +398,7 @@ and p.aktif = 'N'
      * @return mixed
      */
     public function actionUpdate($id) {
-        $model = $this->findModel($id);
+        $model = $this->findModel($id);		
         //$model->nama_izin = $model->izin->nama;
         if ($model->loadAll(Yii::$app->request->post()) && $model->saveAll()) {
             Perizinan::updateAll(['tanggal_expired' => $model->tanggal_berlaku_str, 'update_by' => Yii::$app->user->identity->id, 'update_date' => date("Y-m-d")], ['id' => $model->perizinan_id]);
