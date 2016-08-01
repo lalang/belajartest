51a8fb7340f7a720927ab5671785c0e5e4b028d0
Eko Handriyanto <Eko Handriyanto@Lenoko>
2016-08-01 13:37:14 +0700

    repgen

--- backend/views/repgen/index.php
+++ backend/views/repgen/index.php
@@ -1,123 +1,298 @@
 <?php
 
 use yii\helpers\Html;
-use kartik\widgets\DatePicker;
+use yii\widgets\Pjax;
+
 use kartik\export\ExportMenu;
 use kartik\grid\GridView;
-use backend\models\JenisIzin;
-use yii\helpers\ArrayHelper;
+use kartik\widgets\DatePicker;
+use kartik\widgets\Select2;
 
-$this->title = Yii::t('app', 'Report Generator');
-$this->params['breadcrumbs'][] = $this->title;
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\repgenSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = '<span class="fa fa-cogs"></span><small><cite>   Report Generator</cite></small>';
+$this->params['breadcrumbs'][] = 'Report Generator';
 $search = "";
 $this->registerJs($search);
 ?>
-<div class="box box-default">
-    <form method="post" action="repgen\apply">
-        <div class="box-header with-border">
-            <h3 class="box-title">Parameter</h3>
-            <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-            </div>
-        </div>
+<?php Pjax::begin(); ?>
 
-        <div class="box-body">
-            <div class="row">
-                <div class="form-group">
-                    <div class="col-md-4">
-                        <label class="control-label">Pilih Perizinan</label>
-                        <?php
-                        $jenisizin = JenisIzin::find()->all();
-                        $listData = ArrayHelper::map($jenisizin, 'id', 'nama');
-                        ?>
-                        <?= Html::dropDownList(
-                          'jenisizin',
-                          '1',
-                          $listData,
-                          ['id'=>'jenisizin', 'class'=>'form-control select2 select2-hidden-accessible']
-                        )?>
-                    </div>
-                    <div class="col-md-8">
-                        <label class="control-label">Rentang Tanggal</label>
-                        <?php 
-                        echo DatePicker::widget([
-                            'name' => 'from_date',
-                            'value' => date('Y-m-d'),
-                            'type' => DatePicker::TYPE_RANGE,
-                            'name2' => 'to_date',
-                            'value2' => date('Y-m-d'),
-                            'pluginOptions' => [
-                                'autoclose' => true,
-                                'format' => 'yyyy-mm-dd'
-                            ]
-                        ]);
-                        ?>
+<div class="row">
+
+<?= Html::beginForm([''], 'post', ['data-pjax' => '']); ?>
+
+    <div class="col-md-12">
+        <div class="box box-warning collapsed-box">
+            <div class="box-header with-border">
+                <h3 class="box-title">View</h3>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                </div>
+                <?php
+                /* <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                </div> */
+                ?>
+            </div>
+
+            <div class="box-body">
+                <div class="row">
+                    <div class="form-group">
+                        <div class="col-md-12">
+                            <label class="control-label">Perizinan</label>
+                            <?php
+                            if (!$vJenisIzin) {
+                                $vJenisIzin = 'siup';
+                            }
+                            ?>
+                            <?= Html::dropDownList(
+                              'jenisizin',
+                              $vJenisIzin,
+                              $vlistIzin,
+                              ['id'=>'jenisizin', 'class'=>'form-control select2']
+                            )?>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="form-group">
-                    <div class="col-md-4">
-                        <label class="control-label">...</label>
+
+            <div class="box-footer">
+                <input type="submit" value="Apply" class="btn btn-warning" />
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-6">
+            <div class="box box-success collapsed-box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Column</h3>
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
                     </div>
-                    <div class="col-md-8">
-                        <label class="control-label">Wilayah</label>
-                    </div>
+                </div>
+                <div class="box-body">
+                    <?php
+                    $count = count($vlistColumns);
+                    $a = 0;
+                    foreach ($vlistColumns as $key => $column) {
+                        if ($a < 1) {
+                            $firstcolumn = $column;
+                        }
+                        ++$a;
+                    }
+                    if (!$vselect_columns) {
+                        $vselect_columns = $firstcolumn;
+                    }
+                    ?>
+                    <label class="control-label">Select</label>
+                    <?php 
+                    echo Select2::widget([
+                        'name' => 'select_columns',
+                        'value' => $vselect_columns,
+                        'data' => $vlistColumns,
+                        'size' => Select2::SMALL,
+                        'maintainOrder' => true,
+                        'options' => [
+                            'placeholder' => '...', 
+                            'multiple' => true,
+                        ],
+                        'pluginOptions' => [
+                            'allowClear' => true,
+                        ],
+                    ]);
+                    ?>
+                    <label class="control-label">Group</label>
+                    <?php 
+                    $listGroup = array_slice($vlistColumns, 1);
+                    echo Select2::widget([
+                        'name' => 'select_group',
+                        'value' => $vselect_group,
+                        'data' => $listGroup,
+                        'size' => Select2::SMALL,
+                        'maintainOrder' => true,
+                        'options' => ['placeholder' => '...', 'multiple' => true],
+                        'pluginOptions' => [
+                            'allowClear' => true,
+                        ],
+                    ]);
+                    ?>
+                    <label class="control-label">Sort</label>
+                    <?php 
+                    echo Select2::widget([
+                        'name' => 'select_order',
+                        'value' => $vselect_order,
+                        'data' => $vlistOrderColumns,
+                        'size' => Select2::SMALL,
+                        'maintainOrder' => true,
+                        'options' => ['placeholder' => '...', 'multiple' => true],
+                        'pluginOptions' => [
+                            'allowClear' => true,
+                        ],
+                    ]);
+                    ?>
+                </div>
+                <div class="box-footer">
+                    <input type="submit" value="Apply" class="btn btn-success" /><span class="pull-right">Column selected: <code><?= count($vselect_columns) ?></code></span>
                 </div>
             </div>
         </div>
+        <div class="col-md-6">
+            <div class="box box-success collapsed-box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Condition</h3>
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+                    </div>
+                    <?php
+                    /* <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    </div> */
+                    ?>
+                </div>
 
-        <div class="box-footer">
-            <input type="submit" class="btn btn-success">Submit</input>
-    <?php echo print_r($dataProvider); ?>
+                <div class="box-body">
+                    <div class="row">
+                        <div class="form-group">
+                            <div class="col-md-4">
+                                <label class="control-label">Status Permohonan</label>
+                                <?php
+                                if (!$vselect_status) {
+                                    $vselect_status = array();
+                                }
+
+                                ?>
+                                <?php 
+                                echo Select2::widget([
+                                    'name' => 'select_status',
+                                    'value' => $vselect_status,
+                                    'data' => $vlistStatus,
+                                    'size' => Select2::SMALL,
+                                    'maintainOrder' => true,
+                                    'options' => ['placeholder' => '...', 'multiple' => true],
+                                    'pluginOptions' => [
+                                        'allowClear' => true,
+                                    ],
+                                ]);
+                                ?>
+                            </div>
+                            <div class="col-md-8">
+                                <label class="control-label">Tanggal terbit SK</label>
+                                <?php 
+                                if (!$vdatepicker_from) {
+                                    $vdatepicker_from = date('Y-m-d');
+                                    $vdatepicker_to = date('Y-m-d');
+                                }
+                                echo DatePicker::widget([
+                                    'name' => 'datepicker_from',
+                                    'value' => $vdatepicker_from,
+                                    'type' => DatePicker::TYPE_RANGE,
+                                    'name2' => 'datepicker_to',
+                                    'value2' => $vdatepicker_to,
+                                    'pluginOptions' => [
+                                        'autoclose' => true,
+                                        'format' => 'yyyy-mm-dd'
+                                    ]
+                                ]);
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <label class="control-label">Lokasi Perizinan (DKI Jakarta)</label>
+                                <?php
+                                if (!$vselect_lokasi) {
+                                    $vselect_lokasi = array();
+                                }
+
+                                ?>
+                                <?php 
+                                echo Select2::widget([
+                                    'name' => 'select_lokasi',
+                                    'value' => $vselect_lokasi,
+                                    'data' => $vlistLokasi,
+                                    'size' => Select2::SMALL,
+                                    'maintainOrder' => true,
+                                    'options' => ['placeholder' => '...', 'multiple' => true],
+                                    'pluginOptions' => [
+                                        'allowClear' => true,
+                                    ],
+                                ]);
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="box-footer">
+                    <input type="submit" value="Apply" class="btn btn-success" />
+                </div>
+            </div>
         </div>
-    </form>
+    </div>
 
-    <?php
-    /*
-    $gridColumn = [
-        ['class' => 'yii\grid\SerialColumn'],
-        ['attribute' => 'id', 'hidden' => true],
-        'nama',
-        [
-            'class' => 'yii\grid\ActionColumn',
-        ],
-    ]; 
-    ?>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => $gridColumn,
-        'pjax' => true,
-        'pjaxSettings' => ['options' => ['id' => 'kv-pjax-container']],
-        'panel' => [
-            'type' => GridView::TYPE_PRIMARY,
-            'heading' => '<h3 class="panel-title"><i class="glyphicon glyphicon-book"></i>  ' . Html::encode($this->title) . ' </h3>',
-        ],
-        // set a label for default menu
-        'export' => [
-            'label' => 'Page',
-            'fontAwesome' => true,
-        ],
-        // your toolbar can include the additional full export menu
-        'toolbar' => [
-            '{export}',
-            ExportMenu::widget([
-                'dataProvider' => $dataProvider,
-                'columns' => $gridColumn,
-                'target' => ExportMenu::TARGET_BLANK,
-                'fontAwesome' => true,
-                'dropdownOptions' => [
-                    'label' => 'Full',
-                    'class' => 'btn btn-default',
-                    'itemsBefore' => [
-                        '<li class="dropdown-header">Export All Data</li>',
-                    ],
-                ],
-            ]) ,
-        ],
-    ]); 
-    */ 
-    ?>
-
+<?= Html::endForm() ?>
+        
 </div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h3 class="box-title">Result</h3>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                </div>
+            </div>
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php 
+                        $gridColumn = [['class' => 'yii\grid\SerialColumn']];
+                        foreach($vselect_columns as $value) {
+                            array_push($gridColumn, $value);
+                        }
+                        echo GridView::widget([
+                            'dataProvider' => $dataProvider,
+                            'columns' => $gridColumn,
+                            'showOnEmpty' => false,
+                            'hover' => true,
+                            'condensed' => true,
+                            'perfectScrollbar' => true,
+                            'pjax' => true,
+                            'pjaxSettings' => ['options' => ['id' => 'kv-pjax-container-repgen']],
+                            'panel' => [
+                                'type' => GridView::TYPE_DEFAULT,
+                            ],
+                            // your toolbar can include the additional full export menu
+                            'toolbar' => [
+                                ExportMenu::widget([
+                                    'dataProvider' => $dataProvider,
+                                    'columns' => $gridColumn,
+                                    'target' => ExportMenu::TARGET_BLANK,
+                                    'fontAwesome' => true,
+                                    'dropdownOptions' => [
+                                        'label' => 'Full',
+                                        'class' => 'btn btn-default',
+                                        'itemsBefore' => [
+                                            '<li class="dropdown-header">Export All Data</li>',
+                                        ],
+                                    ],
+                                ]) ,
+                            ],
+                        ]); 
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="box-footer">
+                <span>Record count: <code><?= $dataProvider->getTotalCount() ?></code></span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php Pjax::end()?>
